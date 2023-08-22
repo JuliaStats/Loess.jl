cd3d5d68f70c56386da0e75946b4b8fe9da664c8
Phillip Alday <phillip.alday@beacon.bio>
2023-08-22 13:41:47 -0500

    fix

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -4,11 +4,9 @@ const SUITE = BenchmarkGroup()
 
 SUITE["random"] = BenchmarkGroup()
 
-SUITE["random"] = begin
-    for i in 2:3
-        n = 10^i
-        x = rand(MersenneTwister(42), n)
-        y = sqrt.(x)
-        @benchmarkable loess($x, $y)
-    end
+for i in 2:3
+    n = 10^i
+    x = rand(MersenneTwister(42), n)
+    y = sqrt.(x)
+    SUITE["random"][string(n)] = @benchmarkable loess($x, $y)
 end
