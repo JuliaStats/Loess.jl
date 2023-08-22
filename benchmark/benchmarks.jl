c2ef5977c00c641bf1a5c8eed088650dc9397ad3
Phillip Alday <palday@users.noreply.github.com>
2023-08-22 20:14:48 +0000

    Add benchmark GHA (#79)
    * benchmark CI
    
    ---------
    
    Co-authored-by: Alex Arslan <ararslan@comcast.net>

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,19 @@
+using BenchmarkTools, Loess, Random
+
+const SUITE = BenchmarkGroup()
+
+SUITE["random"] = BenchmarkGroup()
+
+for i in 2:6
+    n = 10^i
+    x = rand(MersenneTwister(42), n)
+    y = sqrt.(x)
+    SUITE["random"][string(n)] = @benchmarkable loess($x, $y)
+end
+
+SUITE["ties"] = BenchmarkGroup()
+let
+    x = repeat([π/4*i for i in -20:20], inner=101)
+    y = sin.(x)
+    SUITE["ties"]["sine"] = @benchmarkable loess($x, $y; span=0.2)
+end
